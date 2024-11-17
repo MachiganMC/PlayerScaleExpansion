f92a3595cf4b0844ee5f778cfddd1a42b721ec7d
MachiganMC <machigan.viral510@passinbox.com>
2024-11-17 18:29:21 +0100

    chore: init project

---
+++ src/main/java/be/machigan/PlayerScaleExpansion.java
@@ -0,0 +1,39 @@
+package be.machigan;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class PlayerScaleExpansion extends PlaceholderExpansion {
+    @Override
+    public @NotNull String getIdentifier() {
+        return "player-scale";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "Machigan";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer p, @NotNull String params) {
+        if (p == null || !p.isOnline()) return "";
+        AttributeInstance scale = ((Player) p).getAttribute(Attribute.GENERIC_SCALE);
+        if (scale == null) return "";
+        return String.valueOf(scale.getValue());
+    }
+}
